98ffa064c46f9063e421cd4372ec14a0db082c39
秋声赋 <unhappy224@163.com>
2016-04-21 16:01:35 +0800

    更换为无IDFA版本  以降低审核无法通过的几率

--- UMeng.Touch.Analytics/UMeng.Touch.Analytics/libMobClickLibrary_3_6_6.linkwith.cs
+++
@@ -1,3 +0,0 @@
-using ObjCRuntime;
-
-[assembly: LinkWith ("libMobClickLibrary_3_6_6.a", SmartLink = true, ForceLoad = true)]
---
+++ UMeng.Touch.Analytics/UMeng.Touch.Analytics/libMobClickLibrary_3_6_7.linkwith.cs
@@ -0,0 +1,3 @@
+using ObjCRuntime;
+
+[assembly: LinkWith ("libMobClickLibrary_3_6_7.a", SmartLink = true, ForceLoad = true)]
